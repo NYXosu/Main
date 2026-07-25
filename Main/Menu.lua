82265163d2405879b48e209948366a906a1fa57d
NYXosu <nyx.osuyt@gmail.com>
2026-07-24 21:52:22 -0500

    Update Menu.lua

--- Main/Menu.lua
+++ Main/Menu.lua
@@ -1,1 +1,16 @@
--- nothing
+local ID = game.PlaceId
+
+local URLs = {
+    [6403373529] = "https://raw.githubusercontent.com/NYXosu/Main/main/Main/2380077519/SlapBattlesR3O.lua",
+    [9426795465] = "https://raw.githubusercontent.com/NYXosu/Main/main/Main/2380077519/MatchMakingR3O.lua",
+    [9015014224] = "https://raw.githubusercontent.com/NYXosu/Main/main/Main/2380077519/SlapRoyaleR3O.lua"
+}
+
+local URL = URLs[ID]
+
+if URL then
+    loadstring(game:HttpGet(URL))()
+    
+else
+    warn("Game Not Supported")
+end
